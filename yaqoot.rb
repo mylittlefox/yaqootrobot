8d8c8ab523ccdfa433706676b02efdb9d9c58026
mylittlefox <500.tayefi@gmail.com>
2016-06-25 15:00:02 +0430

    Update yaqoot.rb

--- yaqoot.rb
+++ yaqoot.rb
@@ -5,7 +5,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/yaqoot'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hi!")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hi!!")
     end
   end
 end
